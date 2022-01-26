f64f69210b1d875f7e562c4b36fc6d35b3091a4d
Purvi2401 <77976203+Purvi2401@users.noreply.github.com>
2022-01-26 18:27:59 +0530

    added dc id & phone no fields

--- kc/quiz/models.py
+++ kc/quiz/models.py
@@ -6,12 +6,20 @@ class Team(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     participant1 = models.CharField(max_length=200, blank=True)
     participant1_email = models.EmailField(max_length=254, blank=True)
+    participant1_dc = models.CharField(max_length=100, blank=True)
+    participant1_phone = models.CharField(max_length=20, blank=True)
     participant2 = models.CharField(max_length=200, blank=True)
     participant2_email = models.EmailField(max_length=254, blank=True)
+    participant2_dc = models.CharField(max_length=100, blank=True)
+    participant2_phone = models.CharField(max_length=20, blank=True)
     participant3 = models.CharField(max_length=200, blank=True)
     participant3_email = models.EmailField(max_length=254, blank=True)
+    participant3_dc = models.CharField(max_length=100, blank=True)
+    participant3_phone = models.CharField(max_length=20, blank=True)
     participant4 = models.CharField(max_length=200, blank=True)
     participant4_email = models.EmailField(max_length=254, blank=True)
+    participant4_dc = models.CharField(max_length=100, blank=True)
+    participant4_phone = models.CharField(max_length=20, blank=True)
 
     def ___str___(self):
         return self.user.username
--- kc/quiz/serializers.py
+++ kc/quiz/serializers.py
@@ -19,12 +19,20 @@ class TeamRegisterSerializer(serializers.ModelSerializer):
             "user",
             "participant1",
             "participant1_email",
+            "participant1_dc",
+            "participant1_phone",
             "participant2",
             "participant2_email",
+            "participant2_dc",
+            "participant2_phone",
             "participant3",
             "participant3_email",
+            "participant3_dc",
+            "participant3_phone",
             "participant4",
             "participant4_email",
+            "participant4_dc",
+            "participant4_phone",
         )
 
     def create(self, data):
@@ -35,12 +43,20 @@ class TeamRegisterSerializer(serializers.ModelSerializer):
             user=user,
             participant1=data["participant1"],
             participant1_email=data["participant1_email"],
+            participant1_dc=data["participant1_dc"],
+            participant1_phone=data["participant1_phone"],
             participant2=data["participant2"],
             participant2_email=data["participant2_email"],
+            participant2_dc=data["participant2_dc"],
+            participant2_phone=data["participant2_phone"],
             participant3=data["participant3"],
             participant3_email=data["participant3_email"],
+            participant3_dc=data["participant3_dc"],
+            participant3_phone=data["participant3_phone"],
             participant4=data["participant4"],
             participant4_email=data["participant4_email"],
+            participant4_dc=data["participant4_dc"],
+            participant4_phone=data["participant4_phone"],
         )
         team.save()
         return user
